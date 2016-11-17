73dcec8bbe776379bc4f78103c5bb27aa8261a0f
damien <damien@chansel.info>
2016-11-17 16:38:01 +0100

    Correct the WP_CONTENT_URL constant

--- wp-config/wp-config.default.php
+++ wp-config/wp-config.default.php
@@ -65,6 +65,6 @@ define('WP_MEMORY_LIMIT', '64M');
 
 define('WP_HOME',  $protocol . $_SERVER['HTTP_HOST'] . '/wordpress');
 define('WP_CONTENT_DIR', realpath(ABSPATH . '../wp-content/'));
-define('WP_CONTENT_URL', WP_HOME . '/wp-content');
+define('WP_CONTENT_URL', $protocol . $_SERVER['HTTP_HOST'] . '/wp-content');
 define('WP_SITEURL',  $protocol . $_SERVER['HTTP_HOST'] . '/wordpress');
 define('UPLOADS', '../wp-uploads');
